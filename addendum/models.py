15463261d0ad27e314c1f54ffa5741ed43d59306
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2013-03-28 18:37:15 -0400

    Adds caching for snippets

--- addendum/models.py
+++ addendum/models.py
@@ -1,4 +1,21 @@
+from django.core.cache import cache
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+
+class CachedManager(models.Manager):
+
+    def get_from_cache(self, id):
+        """
+        Fetches the snippet from cache, and if for some reason it's missing,
+        add it to the cache after retrieval.
+        """
+        snippet = cache.get('snippet:{0}'.format(id))
+        if not snippet:
+            snippet = Snippet.objects.get(id=id)
+            cache.set('snippet:{0}'.format(id), snippet)
+        return snippet
 
 
 class Snippet(models.Model):
@@ -7,9 +24,24 @@ class Snippet(models.Model):
     """
     id = models.CharField(max_length=100, primary_key=True)
     text = models.TextField()
+    objects = CachedManager()
 
     class Meta:
         ordering = ('id',)
 
     def __unicode__(self):
         return self.id
+
+
+@receiver(post_save, sender=Snippet)
+def set_cached_business(sender, **kwargs):
+    """Update the cached copy of the snippet on creation or change"""
+    instance = kwargs.pop('instance')
+    cache.set('snippet:{0}'.format(instance.id), instance)
+
+
+@receiver(post_delete, sender=Snippet)
+def clear_cached_business(sender, **kwargs):
+    """Remove the cached copy of the snippet after deletion"""
+    instance = kwargs.pop('instance')
+    cache.delete('snippet:{0}'.format(instance.id))
--- example/test_project/settings.py
+++ example/test_project/settings.py
@@ -20,6 +20,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
