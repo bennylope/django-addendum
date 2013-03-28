98273524832bac736bce9877796ae00b7a09c4d5
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2013-03-28 18:42:39 -0400

    Adds South migrations

--- example/test_project/settings.py
+++ example/test_project/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'addendum',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
