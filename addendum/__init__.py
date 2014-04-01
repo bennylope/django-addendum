cb601c7e51411796a3323e6029f0860ea5e73452
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-04-01 12:50:46 -0400

    Test more Python & Django versions
    Still running into issues with the app registry on Django 1.7

--- addendum/__init__.py
+++ addendum/__init__.py
@@ -1,2 +1,5 @@
 __title__ = "addendum"
 __version__ = "0.1.0"
+
+
+default_app_config = 'addendum.apps.AddendumConfig'
---
+++ addendum/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AddendumConfig(AppConfig):
+    name = 'addendum'
+    verbose_name = "Addendum"
